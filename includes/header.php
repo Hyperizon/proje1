e9718de21ba2fd48f585c93fddf917dceaf375ae
Hyperizon <oguzhan___kaplan@hotmail.com>
2016-12-03 23:22:09 +0300

    PROJE1

---
+++ includes/header.php
@@ -0,0 +1,26 @@
+<html>
+  <head>
+    <title>ÖDEV</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="styles/style.css">
+  </head>
+
+
+  <body>
+
+    <div id="logo">
+
+    </div>
+    <div id="menu">
+      <ul>
+        <li><a href="index.php">Anasayfa</a></li>
+
+        <li><a href="index.php">Hakkımızda</a></li>
+
+        <li><a href="index.php">Referanslar</a></li>
+
+        <li><a href="index.php">İleteşim</a></li>
+      </ul>
+    </div>
+
+    <div class="site_genisligi">
